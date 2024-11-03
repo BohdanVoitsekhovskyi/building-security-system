32ba2e9438077a85f1d4d34122740b2aed83c9d1
BohdanVoitsekhovskyi <bohdanvoitsex@gmail.com>
2024-11-03 12:47:39 +0200

    add frontend branch

---
+++ backend/building-security-system/src/main/java/com/building/security/system/BuildingSecuritySystemApplication.java
@@ -0,0 +1,13 @@
+package com.building.security.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingSecuritySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingSecuritySystemApplication.class, args);
+	}
+
+}
