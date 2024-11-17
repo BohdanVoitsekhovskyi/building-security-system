33b58489ec8205d89d06d14f8721eb42d6ff6e0e
= <oles.tarasiuk.pz.2022@lpnu.ua>
2024-11-17 20:40:30 +0200

    Added auth services

--- frontend/src/app/pages/account/login/login.component.ts
+++ frontend/src/app/pages/account/login/login.component.ts
@@ -1,22 +1,41 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 import { LoginInfo } from './login.model';
+import { AuthService } from '../../../services/auth/auth.service';
+import { LoadSpinnerComponent } from "../../../shared/load-spinner/load-spinner.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, LoadSpinnerComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  isLoading: boolean = false;
+
   loginInfo: LoginInfo = {
     email: '',
     password: '',
   };
 
-  onSubmit() {
-    console.log(this.loginInfo);
+  onSubmit(form: NgForm) {
+    if (form.invalid) return;
+
+    this.isLoading = true;
+    this.authService.login(this.loginInfo).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.isLoading = false;
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        console.log(err);
+        this.isLoading = false;
+      }
+    });
   }
 }
--- frontend/src/app/utils/header/header.component.ts
+++ frontend/src/app/utils/header/header.component.ts
@@ -2,15 +2,20 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { PopupService } from '../info-popup/popup.service';
 import { PopupInfo } from '../info-popup/popup.model';
+import { AuthService } from '../../services/auth/auth.service';
+import { UserProfileComponent } from "../../shared/user-profile/user-profile.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, UserProfileComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  private authService = inject(AuthService);
+  isAuthenticated = this.authService.isLoggedIn;
+
   private popupService = inject(PopupService);
 
   showPopup() {
