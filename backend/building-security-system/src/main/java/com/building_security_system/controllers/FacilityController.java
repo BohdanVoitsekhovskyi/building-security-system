20a025dee290db090701c3c40d637e9d02369cfc
Roman Osadchyi <romanos4740@gmail.com>
2024-11-17 15:45:51 +0200

    Added cross origin

--- backend/building-security-system/src/main/java/com/building_security_system/controllers/FacilityController.java
+++ backend/building-security-system/src/main/java/com/building_security_system/controllers/FacilityController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/")
+@CrossOrigin
 public class FacilityController {
     private final FacilityService facilityService;
 
--- backend/building-security-system/src/main/java/com/building_security_system/controllers/TestingController.java
+++ backend/building-security-system/src/main/java/com/building_security_system/controllers/TestingController.java
@@ -1,9 +1,11 @@
 package com.building_security_system.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class TestingController {
 }
