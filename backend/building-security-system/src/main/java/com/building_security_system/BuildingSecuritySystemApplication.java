b91ed93ceba756afd496c01b08893170e6294e8a
BohdanVoitsekhovskyi <bohdanvoitsex@gmail.com>
2024-11-18 16:56:32 +0200

    backend2 first commit

--- backend/building-security-system/src/main/java/com/building_security_system/BuildingSecuritySystemApplication.java
+++ backend/building-security-system/src/main/java/com/building_security_system/BuildingSecuritySystemApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BuildingSecuritySystemApplication {
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(BuildingSecuritySystemApplication.class, args);
 	}
