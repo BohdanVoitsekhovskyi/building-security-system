d242bee2f76cfc97cae1b6b28634f0e6c7f2eb08
Roman Osadchyi <romanos4740@gmail.com>
2024-11-20 23:43:47 +0200

    Enums introduced instead of strings for detectorType

---
+++ backend/building-security-system/src/main/java/com/building_security_system/dto/DetectorDto.java
@@ -0,0 +1,15 @@
+package com.building_security_system.dto;
+
+import com.building_security_system.models.Position;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class DetectorDto {
+    private long id;
+    private String type;
+    private Position position;
+}
