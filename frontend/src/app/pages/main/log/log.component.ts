6b6ea5aff6224259d26da7866c0d37929b111314
Niki <nikmisch2005@gmail.com>
2024-11-25 18:32:49 +0200

    added logger view

--- frontend/src/app/pages/main/log/log.component.ts
+++ frontend/src/app/pages/main/log/log.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavbarComponent } from "../../../utils/navbar/navbar.component";
+import { TesterService } from '../../../services/tester.service';
+import { SystemReaction } from '../../../models/system-reaction.model';
+import { Subscription } from 'rxjs';
+import { DetailedSystemReactionComponent } from "./detailed-system-reaction/detailed-system-reaction.component";
 
 @Component({
   selector: 'app-log',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, DetailedSystemReactionComponent],
   templateUrl: './log.component.html',
   styleUrl: './log.component.css'
 })
 export class LogComponent {
+  private testerService = inject(TesterService);
+  systemReactions: SystemReaction[] = [];
 
+  socket?: Subscription;
+
+  constructor() {
+    this.socket = this.testerService.onLog().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.systemReactions = [...this.systemReactions, data];
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.socket?.unsubscribe();
+  }
+
+  onExport() {
+    //TODO
+  }
 }
