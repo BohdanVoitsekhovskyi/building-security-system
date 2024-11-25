f5da449c1734e91c2094cfd318b4deedd894fd93
Niki <nikmisch2005@gmail.com>
2024-11-25 23:58:57 +0200

    addet floors to logger reaction

--- frontend/src/app/pages/main/log/detailed-system-reaction/detailed-system-reaction.component.ts
+++ frontend/src/app/pages/main/log/detailed-system-reaction/detailed-system-reaction.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, inject, Input } from '@angular/core';
 import { SystemReaction } from '../../../../models/system-reaction.model';
 import { CommonModule } from '@angular/common';
+import { FacilityService } from '../../../../services/facility.service';
+import { DetectorReaction } from '../../../../models/detector-reaction.model';
 
 @Component({
   selector: 'app-detailed-system-reaction',
@@ -11,4 +13,12 @@ import { CommonModule } from '@angular/common';
 })
 export class DetailedSystemReactionComponent {
   @Input({ required: true }) systemReaction!: SystemReaction;
+  private facilityService = inject(FacilityService);
+
+  getFloorNumber(reaction: DetectorReaction) {
+    const floor =  this.facilityService.facility()?.floors.find((floor) => 
+     floor.detectors.some(d => d.id === reaction.detector.id )
+    )
+    return floor?.floorNumber
+  }
 }
